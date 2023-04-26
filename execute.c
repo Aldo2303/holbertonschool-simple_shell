7736a1d803df074ee2164b6fe16a0cebd28bdef4
Aldo2303 <aldod03@gmail.com>
2023-04-26 09:03:59 -0700

    changes

---
+++ execute.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+/**
+ * execute - creates a child process and executes.
+ * @command: command to be executed.
+ * @command_cpy: @command duplicated.
+ * @av: each word of command.
+ * @path: PATH.
+ * Return: if couldn't create child proccess -1, 0 if success.
+ */
+int execute(char *command, char *command_cpy, char **av, char *path)
+{
+	int status = 0;
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Error");
+		return (-1);
+	}
+	if (!pid)
+	{
+		execve(av[0], av, environ);
+		perror("Shell");
+		free(command), free(command_cpy), free(av), free(path);
+		exit(EXIT_FAILURE);
+	}
+	wait(&status);
+	free(path);
+	return (0);
+}
+
+/**
+ * eway - checks for '/' in @av[0], and exits if @av[0] is "exit".
+ * @cmd: command to be executed.
+ * @cmdcpy: @command duplicated.
+ * @av: each word of command.
+ * @path: PATH.
+ * Return: calls the concerned function.
+ */
+int eway(char *cmd, char *cmdcpy, char **av, char *path)
+{
+	struct stat st;
+	int i;
+
+	if (!strcmp(av[0], "exit"))
+	{
+		free(cmd), free(cmdcpy), free(av), free(path);
+		exit(EXIT_SUCCESS);
+	}
+	for (i = 0; cmd[i]; i++)
+	{
+		if (cmd[i] == 47 || cmd[i] == 91)
+		{
+			if (!stat(av[0], &st))
+			{
+				return (execute(cmd, cmdcpy, av, path));
+			}
+			execve(av[0], av, environ);
+			perror("Shell");
+			free(path);
+			return (0);
+		}
+	}
+	return (exec_no_path(av, path, cmdcpy, cmd));
+}
+
+/**
+ * exec_no_path - adds the route of the command.
+ * @cmd: command to be executed.
+ * @cmdcpy: @command duplicated.
+ * @av: each word of command.
+ * @path: PATH.
+ * Return: calls to execute if the command exists, 0 if not.
+ */
+int exec_no_path(char **av, char *path, char *cmdcpy, char *cmd)
+{
+	char *where = findcmd(av[0], path);
+
+	av[0] = where;
+
+	if (where)
+	{
+		return (execute(cmd, cmdcpy, av, path));
+	}
+	execve(av[0], av, environ);
+	perror("Shell");
+	free(path);
+	return (0);
+}
---
+++ token.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * tokenizer - splits an array into tokens.
+ * @command: array to be splited.
+ * @delim: array of delimiters.
+ * Return: splited array.
+ */
+char **tokenizer(char *command, char *delim)
+{
+	int i, argc;
+	char **argv = NULL;
+	char *command_cpy = strdup(command), *token = strtok(command_cpy, delim);
+
+	for (argc = 1; token; argc++)
+		token = strtok(NULL, delim);
+
+	argv = malloc(sizeof(char *) * argc);
+	if (argv == NULL)
+		return (NULL);
+
+	free(command_cpy);
+	token = strtok(command, delim);
+
+	for (i = 0; token; i++)
+	{
+		argv[i] = token;
+		token = strtok(NULL, delim);
+	}
+	argv[i] = NULL;
+
+	return (argv);
+}
